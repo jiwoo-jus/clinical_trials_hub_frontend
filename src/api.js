12db943d2f4823e8f66af3076a8d1a26225517d3
jiwoo-jus <parkjiwoo9980@gmail.com>
2025-04-03 21:40:59 -0400

    Refactor: Update API baseURL Setting to use environment variable

--- src/api.js
+++ src/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 // Adjust baseURL if your backend is on a different port or domain
+// For Development Environment, REACT_APP_API_URL is defined in root folder .env file 
+// (e.g. .env > REACT_APP_API_URL=http://localhost:5050/api)
+// For Production Environment, REACT_APP_API_URL is defined in the server's environment variables. 
+// (e.g. Vercel.com Environment Variables Setting > REACT_APP_API_URL: https://clinical-trials-hub-backend.onrender.com/api)
 const api = axios.create({
-  baseURL: 'http://localhost:5050/api'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
